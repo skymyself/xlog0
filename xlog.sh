6d57dc506b8a3298367d2c4e5c333f19097ec79b
skymyself <62652944+skymyself@users.noreply.github.com>
2022-11-17 22:16:02 +0800

    Update xlog.sh

--- xlog.sh
+++ xlog.sh
@@ -4,10 +4,10 @@
 
   systemctl restart xray
   sleep 1
-  systemctl status xray
-  exit
   xray api statsquery --server=127.0.0.1:10085
 
-  echo "重置完成"
+  echo "重置日志完成"
+  echo ""
+  echo "检查xray状态"
 
-  exit
+  systemctl status xray
